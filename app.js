c28c0d5d38c0a5d40aba2212a156c75c3aa1e613
Gadrawingz <legmeekmc4@gmail.com>
2024-09-08 11:19:36 +0200

    Basic setup - express + ejs complete

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 // Variables initialization
 const express = require('express');
-const exphbs = require('express-handlebars');
+const eLayout = require('express-ejs-layouts');
 const app = express();
 
-// Action:
-app.engine('handlebars', exphbs.engine({
-    extname: "handlebars",
-    defaultLayout: "main",
-    layoutsDir: __dirname + "/layouts",
-}));
-
-app.set('view engine', 'handlebars');
+// Template engine definition(EJS):
+app.use(eLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
 
 // Route + rendering using EJS
-app.get('/', (request, response) => {
-    response.render("index", {
-        loggedIn: true,
-        username: "Johnson", 
-        title: "Home"
-    })
+app.get('', (request, response) => {
+    const locals = {
+        title: "Homepage | Simple Blog",
+        description: "Simple Blog, created using Node.js"
+    }
+    response.render("index", {locals});
+});
+
+app.get('/about', (request, response) => {
+    const locals = {
+        title: "About Us | Simple Blog",
+        description: "Simple Blog, created using Node.js"
+    }
+    response.render("about", {locals});
+});
+
+app.get('/contact', (request, response) => {
+    response.render("contact");
 });
 
 // Start server
 app.listen(3000, ()=> {
-    console.log("Running on port 3000");
+    console.log("Site is unning on port 3000");
 });
